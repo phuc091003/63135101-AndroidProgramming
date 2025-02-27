80b283654ea654961a820a3ecf34615ac026efbd
phuc091003 <huuphucnguyen73@gmail.com>
2025-02-27 17:32:55 +0700

    Update MainActivity.java

--- Ex4_AddSubMulDiv_Onclick/app/src/main/java/com/example/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/com/example/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -11,22 +11,31 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+    }
+    void TimDieuKhien(){
+        EditText editTextSo1 = (EditText) findViewById(R.id.edtSo1);
+        EditText editTextSo2 = (EditText) findViewById(R.id.edtSo2);
+        EditText editTextKQ = (EditText) findViewById(R.id.edtKetQua);
     }
     void XuLyCong(View v){
         //b1 tìm edittext số 1 và số 2
-        EditText editTextSo1 = (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2 = (EditText) findViewById(R.id.edtSo2);
         //b2 Lấy dữ liệu từ 2 điều khiển đó
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
@@ -35,15 +44,12 @@ public class MainActivity extends AppCompatActivity {
         float soB =  Float.parseFloat(soThu2);
         // tính toán
         float Tong = soA + soB;
-        EditText editTextKQ = (EditText) findViewById(R.id.edtKetQua);
         String chuoiKQ = String.valueOf(Tong);
         editTextKQ.setText(chuoiKQ);
 
     }
     void XuLyTru(View v){
         //b1 tìm edittext số 1 và số 2
-        EditText editTextSo1 = (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2 = (EditText) findViewById(R.id.edtSo2);
         //b2 Lấy dữ liệu từ 2 điều khiển đó
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
@@ -52,15 +58,12 @@ public class MainActivity extends AppCompatActivity {
         float soB =  Float.parseFloat(soThu2);
         // tính toán
         float Tong = soA - soB;
-        EditText editTextKQ = (EditText) findViewById(R.id.edtKetQua);
         String chuoiKQ = String.valueOf(Tong);
         editTextKQ.setText(chuoiKQ);
 
     }
     void XuLyNhan(View v){
         //b1 tìm edittext số 1 và số 2
-        EditText editTextSo1 = (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2 = (EditText) findViewById(R.id.edtSo2);
         //b2 Lấy dữ liệu từ 2 điều khiển đó
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
@@ -69,15 +72,12 @@ public class MainActivity extends AppCompatActivity {
         float soB =  Float.parseFloat(soThu2);
         // tính toán
         float Tong = soA * soB;
-        EditText editTextKQ = (EditText) findViewById(R.id.edtKetQua);
         String chuoiKQ = String.valueOf(Tong);
         editTextKQ.setText(chuoiKQ);
 
     }
     void XuLyChia(View v){
         //b1 tìm edittext số 1 và số 2
-        EditText editTextSo1 = (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2 = (EditText) findViewById(R.id.edtSo2);
         //b2 Lấy dữ liệu từ 2 điều khiển đó
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
@@ -86,9 +86,7 @@ public class MainActivity extends AppCompatActivity {
         float soB =  Float.parseFloat(soThu2);
         // tính toán
         float Tong = soA / soB;
-        EditText editTextKQ = (EditText) findViewById(R.id.edtKetQua);
         String chuoiKQ = String.valueOf(Tong);
         editTextKQ.setText(chuoiKQ);
-
     }
 }
