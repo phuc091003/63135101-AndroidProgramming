7e05e3b5acea0d7e3f78273ff19ed023d0b9c4e6
phuc091003 <huuphucnguyen73@gmail.com>
2025-03-10 12:16:08 +0700

    push

--- Ex6_IntentDonGian/app/src/main/java/com/example/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/com/example/ex6_intentdongian/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ex6_intentdongian;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien(){
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+           
         });
     }
 }
