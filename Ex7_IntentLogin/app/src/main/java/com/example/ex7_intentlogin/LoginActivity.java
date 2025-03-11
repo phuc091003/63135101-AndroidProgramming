ca2519b9ca0c2da9dd5f1c2e0132a5f3c1a1d545
phuc091003 <huuphucnguyen73@gmail.com>
2025-03-11 22:20:20 +0700

    push

--- Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +18,30 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                //xu ly dang nhap
+                EditText edTenDN = (EditText) findViewById(R.id.edtUsername);
+                EditText edPass = (EditText) findViewById(R.id.edtPass);
+                String tenDangNhap = edTenDN.getText().toString();
+                String mk = edPass.getText().toString();
+                //ktra mat khau
+                if( tenDangNhap.equals("phuc091003") && mk.equals("hoilamj12")){
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    iQuiz.putExtra("ten_dang_nhap", tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap", mk);
+                    // gui di
+                    startActivity(iQuiz);
+
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Sai thông tin", Toast.LENGTH_LONG);
+                }
+            }
         });
     }
 }
--- Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.ex7_intentlogin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // tim dk
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //event
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
         });
+
     }
 }
