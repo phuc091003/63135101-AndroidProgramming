fd60f39cc3da733ac2f6ac08028bc36b06c9ef72
phuc091003 <huuphucnguyen73@gmail.com>
2025-03-24 11:13:10 +0700

    push

--- LuyenTapAppTinhToan/app/src/main/java/com/example/luyentapapptinhtoan/MainActivity.java
+++ LuyenTapAppTinhToan/app/src/main/java/com/example/luyentapapptinhtoan/MainActivity.java
@@ -7,18 +7,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCong;
+    Button btnTru;
+    Button btnNhan;
+    Button btnChia;
+    EditText edt1;
+    EditText edt2;
+    EditText edtKetQua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btnCong = findViewById(R.id.btnCong);
+        btnTru = findViewById(R.id.btnTru);
+        btnNhan = findViewById(R.id.btnNhan);
+        btnChia = findViewById(R.id.btnChia);
+        edt1 = findViewById(R.id.edt1);
+        edt2 = findViewById(R.id.edt2);
+        edtKetQua = findViewById(R.id.edtKetQua);
     }
 }
