b08abf8e0d48108b56865fd0e66c4e8b66bbe9f1
phuc091003 <huuphucnguyen73@gmail.com>
2025-03-26 10:18:32 +0700

    push

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/LandScape.java
@@ -0,0 +1,22 @@
+package com.example.recyclerview;
+
+public class LandScape {
+    String landImageFileName;
+    String landCation;
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
+}
