1979486f50b88ef7c685e24a47cf0b9db6538834
phuc091003 <huuphucnguyen73@gmail.com>
2025-03-26 11:31:25 +0700

    Create LandScapeAdapter.java

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/LandScapeAdapter.java
@@ -0,0 +1,52 @@
+package com.example.recyclerview;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import android.content.Context;
+import java.util.ArrayList;
+import android.view.LayoutInflater;
+import com.example.recyclerview.R;
+import com.example.recyclerview.LandScape;
+
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemViewHolder> {
+    Context context;
+    ArrayList<LandScape> lstData;
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_land, parent, false);
+        ItemLandHolder viewholderCreated = new ItemLandHolder(vItem);
+        return viewholderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+        TextView tvCaption;
+        ImageView ivLandscape;
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCaption);
+            ivLandscape = itemView.findViewById(R.id.imageViewLand);
+        }
+    }
+}
