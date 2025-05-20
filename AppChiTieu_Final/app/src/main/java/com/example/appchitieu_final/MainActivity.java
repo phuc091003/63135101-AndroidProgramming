6a2773ca657c891e360ed3abacb7a57b4764e989
phuc091003 <huuphucnguyen73@gmail.com>
2025-05-20 20:34:53 +0700

    push

--- AppChiTieu_Final/app/src/main/java/com/example/appchitieu_final/MainActivity.java
+++ AppChiTieu_Final/app/src/main/java/com/example/appchitieu_final/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.appchitieu_final;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNav = findViewById(R.id.bottom_nav);
+        fab = findViewById(R.id.fab_add);
+
+        // Load mặc định HomeFragment
+        loadFragment(new HomeFragment());
+
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    loadFragment(new HomeFragment());
+                    return true;
+                case R.id.nav_transaction:
+                    loadFragment(new TransactionFragment());
+                    return true;
+                case R.id.nav_statistics:
+                    loadFragment(new StatisticsFragment());
+                    return true;
+                case R.id.nav_profile:
+                    loadFragment(new ProfileFragment());
+                    return true;
+            }
+            return false;
         });
+
+        fab.setOnClickListener(v -> {
+            Toast.makeText(this, "Thêm giao dịch", Toast.LENGTH_SHORT).show();
+            // Sau này mở activity hoặc fragment thêm giao dịch
+        });
+    }
+    
+    private void loadFragment(TransactionFragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
